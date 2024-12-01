6d128a959abe0eb381deb64a265580989d5c450b
Hussein Kasem <hussein@kasem.tech>
2024-12-01 10:45:12 +0100

    Day01: part1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,22 +2,27 @@ package day01
 
 import println
 import readInput
+import kotlin.math.abs
+
+const val DELIMITER = "   "
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val right = input.map { it.split(DELIMITER)[0].toInt() }.sorted()
+        val left  = input.map { it.split(DELIMITER)[1].toInt() }.sorted()
+
+        return right.mapIndexed { index, value ->
+            abs(value - left[index])
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
