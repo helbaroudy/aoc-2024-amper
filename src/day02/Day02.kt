a73cd8489e984587d44b9d823c37759e61169c6f
Hussein Kasem <hussein@kasem.tech>
2024-12-06 20:52:34 +0100

    Day02: Part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -18,18 +18,32 @@ fun main() {
         return true
     }
 
+    fun isSafeDumping(report: List<Int>): Boolean {
+        report.forEachIndexed { index, _ ->
+            val dumpped = report.toMutableList()
+            dumpped.removeAt(index)
+            if (isSafe(dumpped)) return true
+        }
+        return false
+    }
+
     fun part1(input: List<String>): Int =
         input
             .map { report -> report.split(" ").map(String::toInt) }
             .count(::isSafe)
 
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input
+            .map { report -> report.split(" ").map(String::toInt) }
+            .count {
+                isSafe(it) || isSafeDumping(it)
+            }
+
 
     val testInput = readInput("day02/input_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("day02/input")
     part1(input).println()
